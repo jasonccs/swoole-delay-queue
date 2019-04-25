17fbdd5df3e0cf9c1b096c7cb6705ad6a1a6065d
shopme <995200452@qq.com>
2019-04-25 18:31:01 +0800

    :penguin:

--- app/logic/Job.php
+++ app/logic/Job.php
@@ -1,5 +1,5 @@
 <?php
-namespace app\service;
+namespace app\logic;
 
 use app\db\Order;
 use Serve\Colors\Color;
@@ -36,7 +36,7 @@ class Job
      */
     public function business($pdo, $data): void
     {
-//        Color::println("处理业务逻辑 ...", ColorText::YELLOW_FONT);
+        Color::println("处理业务逻辑 ...", ColorText::YELLOW_FONT);
 //         待支付订单,直接取消关闭
         $orderSn = $data['orderSn'];
         (new Order($pdo))->getOrder($orderSn)->cancelled();
