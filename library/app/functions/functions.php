39f43c1b179a4bfc578b866c6a52718779af6420
shopme <995200452@qq.com>
2019-04-26 18:28:10 +0800

    :fire:删除library目录

--- library/app/functions/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-#切记不可删除
-function env($name)
-{
-    return \Serve\Core\Env::get($name) ?? null;
-}
--- library/app/test/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2019/4/2 0002
- * Time: 下午 20:42
- */
-
-$str = [1, 2];
